6372ad5361a811a03e5a947cf47fa9687293fa33
Licantropo3392 <denisalbu287@gmail.com>
2025-05-26 21:16:51 +0200

    Add initial project setup with example C++ program
    Set up the CMake build system and a .gitignore file. Add a simple C++ program that greets users, applies a discount for specific names, and processes product selection.

---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+const short sconto = 10;
+
+const std::string amici[] = {
+    "Marco",
+    "Paolo",
+    "Giuseppe",
+    "Luca",
+    "Mario",
+    "Giorgio",
+    "Giulia",
+    "Giovanni",
+    "Giacomo",
+};
+
+struct Prodotto {
+    std::string nome;
+    float prezzo;
+};
+
+int main() {
+    std::string nome;
+    std::string scelta;
+
+    bool scontato = false;
+
+    std::cout << "Ciao, come ti chiami? ";
+    std::cin >> std::ws >> nome;
+
+    for (short i = 0; i < std::size(amici); i++) {
+        if (nome == amici[i]) {
+            std::cout << "Ciao " << amici[i] << " oggi avrai uno sconto del " << sconto << "%!" << std::endl;
+            scontato = true;
+
+            break;
+        }
+
+        if (i == std::size(amici) - 1) {
+            std::cout << "Benvenuto " << nome << std::endl;
+        }
+    }
+
+    const Prodotto prodotti[] = {
+        { "Coca Cola", 2.50f },
+        { "Fanta", 2.00f },
+        { "Sprite", 1.75f },
+    };
+
+    for (short i = 0; i < std::size(prodotti); i++) {
+        if (scontato) {
+            std::cout << prodotti[i].nome << ": €" << (prodotti[i].prezzo - ((prodotti[i].prezzo * sconto) / 100)) << std::endl;
+        } else {
+            std::cout << prodotti[i].nome << ": €" << prodotti[i].prezzo << std::endl;
+        }
+    }
+
+    std::cout << "Quale scegli? ";
+    std::getline(std::cin >> std::ws, scelta);
+
+    for (short i = 0; i < std::size(prodotti); i++) {
+        if (scelta == prodotti[i].nome) {
+            if (scontato) {
+                std::cout << "Mi devi €" << (prodotti[i].prezzo - ((prodotti[i].prezzo * sconto) / 100)) << std::endl;
+            } else {
+                std::cout << "Mi devi €" << prodotti[i].prezzo << std::endl;
+            }
+
+            break;
+        }
+
+        if (i == std::size(prodotti) - 1) {
+            std::cout << "Non valido!" << std::endl;
+        }
+    }
+
+    return 0;
+}
